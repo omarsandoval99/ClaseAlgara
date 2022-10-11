92a08be11ee6d3d3e4ba3cdd6f47ff3af6543d8e
Yhunn <jybrant.efren@gmail.com>
2022-10-11 08:49:24 -0600

    planeacion de coleccion y funciones

--- lib/dominio/coleccion_juegos.dart
+++ lib/dominio/coleccion_juegos.dart
@@ -1,3 +1,4 @@
+import 'package:xml/xml.dart';
 
 class ColeccionJuegos{
   late final List<String> juegos;
@@ -7,5 +8,31 @@ class ColeccionJuegos{
 
   factory ColeccionJuegos.constructor({required propuestaJuegos, required propuestaFechas}){
     return ColeccionJuegos._(propuestaJuegos, propuestaFechas);
+  }
+
+  obtenerJuegosPorPaginaDesdeXML(XmlDocument document){
+
+  }
+
+  obtenerJuegosTodasLasPaginas(String nick){
+    
+    /*
+      List<String> juegos
+      int paginas =  obtenertotalpaginas (nick);
+      for (x=1;paginas;x++;){
+        var jugadasPagina = fetch https://boardgamegeek.com/xmlapi2/plays?username=benthor&page=$x
+        var coleccionporPagina = obtenerJuegosPorPaginaDesdeXML(jugadasPagina)
+        juegos.add()
+      }
+     */
+  }
+  int obtenerTotalPaginas(String nick){
+    /*
+      fetch https://boardgamegeek.com/xmlapi2/plays?username=$nick
+      int totalJugadas = document getelements "plays" first get attribute total .ceil
+      int pags = (totalJugadas / 10) .ceil
+      return pags
+     */
+    return 0;
   }
 }
