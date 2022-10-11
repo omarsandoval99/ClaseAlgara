7aea584a0f91833ee2aafc4167f00ca6a7e4e48f
Yhunn <jybrant.efren@gmail.com>
2022-10-11 07:55:39 -0600

    renombrar tests

--- test/caracteristicas/data_usuario_test.dart
+++ test/caracteristicas/data_usuario_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_application_1/caracteristicas/data_usuario.dart';
 import 'package:flutter_application_1/dominio/variable_dominio.dart';
 import 'package:flutter_test/flutter_test.dart';
 void main() {
-  group('pruebas para buscar jugadas', (){
-    test('obtener coleccion para benthor', (){
+  group('obtener coleccion para benthor', (){
+    test('benthor tiene juego Aguila Roja', (){
       ChecadorDeJugadasDePrueba checador = ChecadorDeJugadasDePrueba();
 
       expect(checador.obtenerJuegos(NickFormado.constructor("benthor")).juegos, contains("Aguila Roja"));
