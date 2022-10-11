e4bc90a2ef945b2e69d3780cc4ca7163af734805
Yhunn <jybrant.efren@gmail.com>
2022-10-11 07:54:39 -0600

    test de data del usuario

--- test/caracteristicas/data_usuario_test.dart
+++ test/caracteristicas/data_usuario_test.dart
@@ -1,4 +1,12 @@
-
+import 'package:flutter_application_1/caracteristicas/data_usuario.dart';
+import 'package:flutter_application_1/dominio/variable_dominio.dart';
+import 'package:flutter_test/flutter_test.dart';
 void main() {
+  group('pruebas para buscar jugadas', (){
+    test('obtener coleccion para benthor', (){
+      ChecadorDeJugadasDePrueba checador = ChecadorDeJugadasDePrueba();
 
+      expect(checador.obtenerJuegos(NickFormado.constructor("benthor")).juegos, contains("Aguila Roja"));
+    });
+  });
 }
